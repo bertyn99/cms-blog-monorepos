90626c48074bc38ddf6c13d26e78c55fd6f35449
bertyn99 <bboulikou@gmail.com>
2024-02-07 18:25:26 +0100

    Add Sidebar Panel component and update default layout

---
+++ apps/front/components/Sidebar/Panel.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const links = [
+  {
+    label: "Vertical Navigation",
+    to: "/components/vertical-navigation",
+  },
+  {
+    label: "Command Palette",
+    to: "/components/command-palette",
+  },
+  {
+    label: "Table",
+    to: "/components/table",
+  },
+];
+</script>
+<template>
+  <div
+    class="hidden sm:block fixed left-0 top-0 bottom-0 w-64 dark:bg-white/5 backdrop-blur"
+  >
+    <div class="px-4 pt-2 space-y-4">
+      <slot name="top" />
+      <UVerticalNavigation :links="links">
+        <template #default="{ link }">
+          <span class="group-hover:text-primary relative">{{
+            link.label
+          }}</span>
+        </template>
+      </UVerticalNavigation>
+    </div>
+  </div>
+</template>
--- apps/front/layouts/default.vue
+++ apps/front/layouts/default.vue
@@ -1,8 +1,15 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Layout: default
+  <div class="h-screen">
+    <SidebarPanel>
+      <template #top>
+        <div class="flex items-center space-x-2">
+          <UIcon name="i-mdi:cube" />
+          <span class="text-white">John Doe</span>
+        </div>
+      </template>
+    </SidebarPanel>
     <slot />
   </div>
 </template>
