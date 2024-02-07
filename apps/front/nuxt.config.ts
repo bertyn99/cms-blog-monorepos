08ef3c1ef6474f8ed278deb0af17e2c38c7f2aac
bertyn99 <bboulikou@gmail.com>
2024-02-07 10:46:17 +0100

    Update nuxt.config.ts and package.json

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ['@nuxt/ui-pro'],
   modules: ['@nuxt/ui'],
+  ui: {
+    icons: {},
+  }
 })
