43a50320120ad5151152cb9f68f6ea84bbb4fea8
bertyn99 <bboulikou@gmail.com>
2024-02-08 11:44:42 +0100

    Refactor sidebar and default layout components

--- apps/front/components/Sidebar/Panel.vue
+++ apps/front/components/Sidebar/Panel.vue
@@ -1,4 +1,10 @@
 <script lang="ts" setup>
+const defineProps = {
+  links: {
+    type: Array,
+    required: true,
+  },
+};
 const links = [
   {
     label: "Vertical Navigation",
@@ -13,13 +19,18 @@ const links = [
     to: "/components/table",
   },
 ];
+
+
 </script>
 <template>
   <div
-    class="hidden sm:block fixed left-0 top-0 bottom-0 w-64 dark:bg-white/5 backdrop-blur"
-  >
-    <div class="px-4 pt-2 space-y-4">
+    class="flex-col items-stretch relative w-full lg:w-[250px] border-b lg:border-b-0 lg:border-r border-gray-200 dark:border-gray-800 hidden lg:flex">
+
+    <div
+      class="sticky top-0 z-40 h-[--header-height] flex-shrink-0 flex items-center border-b border-gray-200 dark:border-gray-800 px-4 gap-x-4 ">
       <slot name="top" />
+    </div>
+    <div class="px-4 py-4 pt-2 space-y-4">
       <UVerticalNavigation :links="links">
         <template #default="{ link }">
           <span class="group-hover:text-primary relative">{{
--- apps/front/layouts/default.vue
+++ apps/front/layouts/default.vue
@@ -1,16 +1,48 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const links = [
+  {
+    label: "Vertical Navigation",
+    to: "/components/vertical-navigation",
+  },
+  {
+    label: "Command Palette",
+    to: "/components/command-palette",
+  },
+  {
+    label: "Table",
+    to: "/components/table",
+  },
+];
+</script>
 
 <template>
-  <div class="h-screen">
+  <div class="fixed inset-0 flex overflow-hidden">
     <SidebarPanel>
       <template #top>
-        <div class="flex items-center space-x-2">
-          <UIcon name="i-mdi:cube" />
-          <span class="text-white">John Doe</span>
-        </div>
+
+        <UIcon name="i-mdi:cube" />
+        <span class="text-white">John Doe</span>
+
       </template>
     </SidebarPanel>
-    <slot />
+    <div class="flex flex-1 w-full">
+      <div class="flex-col items-stretch relative w-full lg:w-[--width] flex-1 flex">
+        <div
+          class="sticky top-0 z-40 h-[--header-height] flex-shrink-0 flex items-center border-b border-gray-200 dark:border-gray-800 px-4 gap-x-4">
+          <div class="flex items-center justify-between flex-1 gap-x-1.5">
+            <UBreadcrumb :links="links" />
+            <div class="flex items-center gap-x-4">
+              <UButton size="sm" icon="i-mdi:bell" />
+              <UButton size="sm" icon="i-mdi:settings" />
+              <UButton size="sm" icon="i-mdi:account" />
+            </div>
+          </div>
+        </div>
+
+        <slot />
+      </div>
+    </div>
+
   </div>
 </template>
 
