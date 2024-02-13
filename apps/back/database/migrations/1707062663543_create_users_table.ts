4b134355db42b6249e6ff66039bc0669fb26864c
bertyn99 <bboulikou@gmail.com>
2024-02-13 17:24:17 +0100

    Add role column to users table

--- apps/back/database/migrations/1707062663543_create_users_table.ts
+++ apps/back/database/migrations/1707062663543_create_users_table.ts
@@ -9,7 +9,11 @@ export default class extends BaseSchema {
       table.string('full_name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
-
+      table.enu('role', ['USER', 'ADMIN'], {
+        useNative: true,
+        enumName: 'user_role',
+        existingType: false,
+      }).defaultTo('draft')
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
@@ -17,5 +21,7 @@ export default class extends BaseSchema {
 
   async down() {
     this.schema.dropTable(this.tableName)
+    this.schema.raw('DROP TYPE user_role');
+
   }
 }
