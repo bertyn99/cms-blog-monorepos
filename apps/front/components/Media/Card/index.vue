70cf3f8c1cf3c7340376b3c191881081a0f453be
bertyn99 <bboulikou@gmail.com>
2024-03-04 17:51:24 +0100

    Add Media Card component and Media page

---
+++ apps/front/components/Media/Card/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <UCard :ui="customStyle">
+        <div class="h-32 w-36 bg-green-400 ">
+
+        </div>
+
+        <template #footer>
+            <div class="h-8">
+
+            </div>
+        </template>
+    </UCard>
+</template>
+
+<script setup>
+import { _padding } from '#tailwind-config/theme';
+
+
+defineProps({
+    name: String,
+    image: String,
+    link: String,
+})
+
+const customStyle = {
+    body: {
+        padding: "p-0",
+        background: "bg-red-300",
+    },
+    header: {
+        base: "h-32 w-36 bg-red-800",
+
+    },
+    footer: {
+        base: "h-8"
+    }
+}
+</script>
---
+++ apps/front/pages/admin/Media.vue
@@ -0,0 +1,46 @@
+<script setup>
+definePageMeta({
+    name: "Media Librairy",
+});
+
+const file = ref([
+    {
+        folder: "pages",
+        media: [{
+            name: "toto.png",
+            type: "file",
+            size: "1.2kb",
+            date: "2021-10-10",
+            link: "/media/pages/toto.png"
+        }]
+    },
+    {
+        folder: "articles",
+        media: [{
+            name: "tata.png",
+            type: "file",
+            size: "1.2kb",
+            date: "2021-10-10",
+            link: "/media/articles/toto.png"
+        }]
+    },
+    {
+        folder: "default",
+        media: [{
+            name: "titi.png",
+            type: "file",
+            size: "1.2kb",
+            date: "2021-10-10",
+            link: "/media/default/toto.png"
+        }]
+    }
+])
+const FileInPagesFolder = computed(() => file.value.filter(f => f.folder === "pages"))
+const FileInDefaultFolder = computed(() => file.value.filter(f => f.folder === "default"))
+</script>
+
+<template>
+    <div class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+        <MediaCard v-for="f in file" :name="f.folder" :image="f.media[0].link" :link="f.media[0].link" />
+    </div>
+</template>
