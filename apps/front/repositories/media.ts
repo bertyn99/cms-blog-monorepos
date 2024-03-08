226ec8bf3fa7e23189082a12c9b4c32a839e4810
bertyn99 <bboulikou@gmail.com>
2024-03-08 18:01:59 +0100

    Refactor mediaRepository to include folder parameter in getAllFile and getAllFolder

--- apps/front/repositories/media.ts
+++ apps/front/repositories/media.ts
@@ -1,17 +1,23 @@
 import type { $Fetch, NitroFetchRequest, NitroFetchOptions } from "nitropack";
 
 export const mediaRepository = <T>(fetch: $Fetch<T, NitroFetchRequest>) => ({
-  async getAllFile(options?: any) {
+  async getAllFile(folder:string='',options?: any) {
     const mergeOptions = {
       credentials: "include",
+      params:{
+        folder:folder==''?'default':folder
+      },
       ...options,
     };
     return fetch("/media?folder=default", mergeOptions);
   },
 
-  async getAllFolder(options?: any) {
+  async getAllFolder(folder:string='',options?: any) {
     const mergeOptions = {
       credentials: "include",
+      params:{
+        folder:folder
+      },
       ...options,
     };
     return fetch("/media/folder", mergeOptions);
