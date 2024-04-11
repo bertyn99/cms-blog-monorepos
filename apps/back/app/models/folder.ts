70bb6fab56af5428964424f3e471357c6c72f11e
bertyn99 <bboulikou@gmail.com>
2024-04-11 15:58:27 +0200

    refacto implementation nested folder

--- apps/back/app/models/folder.ts
+++ apps/back/app/models/folder.ts
@@ -1,24 +1,37 @@
 import { DateTime } from 'luxon'
-import type { HasMany } from '@adonisjs/lucid/types/relations'
-import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
+import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
 import Media from '#models/media'
 
 export default class Folder extends BaseModel {
-    @column({ isPrimary: true })
-    declare id: number
+  @column({ isPrimary: true })
+  declare id: number
 
-    @column()
-    declare name: string
+  @column()
+  declare name: string
 
-    @column()
-    declare path: string
+  @column()
+  declare path: string
 
-    @hasMany(() => Media)
-    declare media: HasMany<typeof Media>
+  @column()
+  declare parentId: number | null
 
-    @column.dateTime({ autoCreate: true })
-    declare createdAt: DateTime
+  @belongsTo(() => Folder, {
+    foreignKey: 'parentId',
+  })
+  declare parent: BelongsTo<typeof Folder>
 
-    @column.dateTime({ autoCreate: true, autoUpdate: true })
-    declare updatedAt: DateTime
+  @hasMany(() => Folder, {
+    foreignKey: 'parentId',
+  })
+  declare subfolders: HasMany<typeof Folder>
+
+  @hasMany(() => Media)
+  declare media: HasMany<typeof Media>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
 }
